dc47a01de2016eb79a2050e92024bc9195aab3a3
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-12-17 22:59:42 -0300

    começando responsividade

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.scss']
+  styleUrls: ['./hello.component.scss'],
 })
 export class HelloComponent implements OnInit {
+  @Input()
+  title?: string = 'Jailson';
 
-  constructor() { }
+  @Input()
+  Iam?: string = 'Muito prazer esse é meu portfólio, e um pouco sobre mim';
+
+  nameTitles: Array<string> = [
+    'Jaja',
+    'Jailson da silva roth',
+    'Jailson Roth',
+    'Roth Jailson',
+    'Jailson',
+  ];
+
+  aboutMe: Array<string> = [
+    'Tenho 23 anos e estou cursando Engenharia de Software',
+    'Gosto de anime, manga, videogame, ler, ir para academia',
+    'Tenho experiência de 1 ano como Desenvolvimento Web full-stack.',
+    'Conhecimento nas linguagens: Java, TypeScript, Angular, JS e MySQL',
+    'Sempre estou em busca de meu conhecimentos',
+    'Muito prazer esse é meu portfólio, e um pouco sobre mim'
+  ];
 
   ngOnInit(): void {
-
-
-
+    this.changeName();
+    this.changeIam();
   }
 
+  changeName() {
+    let i = 0;
+
+    setInterval(() => {
+      this.title = this.nameTitles[i];
+      i = i === +this.nameTitles.length - 1 ? 0 : i + 1;
+    }, 3000);
+  }
+
+  changeIam() {
+    let i = 0;
+
+    setInterval(() => {
+      this.Iam = this.aboutMe[i];
+      i = i === +this.aboutMe.length - 1 ? 0 : i + 1;
+    }, 6000);
+  }
 }
