4549ee5cf4c64639698b06549b01c3167892a27c
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2026-03-03 18:51:07 -0300

    style: melhora legibilidade da política em modo escuro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { PoliticaComponent } from './components/politica/politica.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'politica', component: PoliticaComponent },
+  { path: 'politoca', redirectTo: 'politica', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
